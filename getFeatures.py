7804a971db48e140966cc7de2a09a8039d41dbd2
Vyom Shrivastava <vyom.shrivastava1@gmail.com>
2018-02-14 12:43:25 -0500

    removed bytecode extraction

--- getFeatures.py
+++ getFeatures.py
@@ -12,18 +12,15 @@ sqlc = SQLContext(sc)
 
 hashFiles = sc.textFile("gs://uga-dsp/project2/files/X_small_train.txt")
 asmFiles = hashFiles.map(lambda x: "gs://uga-dsp/project2/data/asm/"+ x+".asm")
-bytesFiles = hashFiles.map(lambda x: "gs://uga-dsp/project2/data/bytes/"+ x+".bytes")
 
 labels = sc.textFile("gs://uga-dsp/project2/files/y_small_train.txt")
 
 def fun(accum,x):
     return accum+','+x
 asmFileString = asmFiles.reduce(fun)
-bytesFileString = bytesFiles.reduce(fun)
 
 
-rdd1= sc.wholeTextFiles(asmFileString)
-
+rdd1= sc.wholeTextFiles(asmFileString,20)
 
 OpcodesList = sc.textFile("gs://elinor-p2/allOpcodes.txt")
 opcodes= sc.broadcast(OpcodesList.collect())
@@ -65,42 +62,7 @@ fourGramCounts = fourGramRdd.flatMapValues(lambda x: x).map(lambda x: (x,1)).red
 segments = rdd1.zipWithIndex().map(lambda x: (x[1],x[0][1].splitlines())).map(lambda x: (x[0],[re.findall(r'\w+:?(?=:)',word) for word in x[1]])).flatMapValues(lambda x: x).map(lambda x: (x[0],x[1][0])).map(lambda x: (x,1)).reduceByKey(add)
 
 
-bytesRdd= sc.wholeTextFiles(bytesFileString)
 
-
-Bytes = bytesRdd.map(lambda x: x[1].split()).map(lambda x: [word for word in x if len(word)<3]).zipWithIndex().map(lambda x: (x[1],x[0]))
-
-
-bytesDataFrame = sqlc.createDataFrame(Bytes,["docId","bytes"])
-
-
-bytesTwoGram = NGram(n=2, inputCol="bytes", outputCol="2grams")
-bytesDataFrame = bytesTwoGram.transform(bytesDataFrame)
-
-
-bytesThreeGram = NGram(n=3, inputCol="bytes", outputCol="3grams")
-bytesDataFrame = bytesThreeGram.transform(bytesDataFrame)
-
-
-bytesFourGram = NGram(n=4, inputCol="bytes", outputCol="4grams")
-bytesDataFrame = bytesFourGram.transform(bytesDataFrame)
-
-
-bytesTwoGramRdd = bytesDataFrame.select("docId","2grams").rdd.map(tuple)
-bytesThreeGramRdd =bytesDataFrame.select("docId","3grams").rdd.map(tuple)
-bytesFourGramRdd =bytesDataFrame.select("docId","4grams").rdd.map(tuple)
-
-
-bytesOneGramCounts = Bytes.flatMapValues(lambda x: x).map(lambda x: (x,1)).reduceByKey(add)
-
-
-bytesTwoGramCounts = bytesTwoGramRdd.flatMapValues(lambda x: x).map(lambda x: (x,1)).reduceByKey(add)
-
-
-bytesThreeGramCounts = bytesThreeGramRdd.flatMapValues(lambda x: x).map(lambda x: (x,1)).reduceByKey(add)
-
-
-bytesFourGramCounts = bytesFourGramRdd.flatMapValues(lambda x: x).map(lambda x: (x,1)).reduceByKey(add)
 
 
 labelRdd = labels.zipWithIndex().map(lambda x: (x[1],x[0]))
@@ -108,13 +70,13 @@ labelRdd = labels.zipWithIndex().map(lambda x: (x[1],x[0]))
 labelFrame = labelRdd.toDF(["did","label"])
 
 
-allFeatures = sc.union([oneGramCounts,twoGramCounts,threeGramCounts,fourGramCounts,segments,bytesOneGramCounts,bytesTwoGramCounts,bytesThreeGramCounts,bytesFourGramCounts])
+allFeatures = sc.union([oneGramCounts,twoGramCounts,threeGramCounts,fourGramCounts,segments])
 
 
 allFeatures = allFeatures.reduceByKey(add).map(lambda x: (x[0][1],(x[0][0],x[1])))
 
 
-vocab = allFeatures.keys().distinct().zipWithIndex()
+vocab = allFeatures.map(lambda x: (x[0],1)).reduceByKey(add).map(lambda x: x[0]).zipWithIndex()
 
 
 allFeaturesJoined = allFeatures.join(vocab).map(lambda x: (x[1][0][0],x[1][1],x[1][0][1]))
